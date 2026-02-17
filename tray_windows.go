669ca93ad5eedf1a589aaff1de6a81d9620277ba
STC214 <2833104901@qq.com>
2026-02-17 16:30:08 +0800

    '托盘点击逻辑优化，现在可以双击托盘图标了'

--- tray_windows.go
+++ tray_windows.go
@@ -4,14 +4,25 @@ package main
 
 import (
 	_ "embed"
+	"sync"
+	"syscall"
 	"time"
 
 	"github.com/getlantern/systray"
+	"github.com/lxn/win"
 	"github.com/wailsapp/wails/v2/pkg/runtime"
 )
 
 //go:embed build/windows/icon.ico
 var trayIcon []byte
+
+var (
+	trayHookMu      sync.Mutex
+	trayHookReady   bool
+	trayHookOrigWnd uintptr
+	trayHookApp     *App
+	trayWndProcPtr  = syscall.NewCallback(trayWindowProc)
+)
 
 func (a *App) initSystemTray() {
 	go systray.Run(func() {
@@ -45,6 +56,7 @@ func (a *App) initSystemTray() {
 		}()
 	}, func() {})
 
+	go a.installTrayDoubleClickRestoreHook()
 	go a.minimiseToTrayLoop()
 }
 
@@ -60,4 +72,60 @@ func (a *App) minimiseToTrayLoop() {
 			runtime.Hide(a.ctx)
 		}
 	}
+}
+
+func (a *App) installTrayDoubleClickRestoreHook() {
+	const (
+		tries             = 80
+		wmSystrayMessage  = 0x0401 // WM_USER + 1, consistent with getlantern/systray windows impl
+		wmLButtonDblClk   = 0x0203
+		classNameSystray  = "SystrayClass"
+		hookRetryInterval = 250 * time.Millisecond
+	)
+
+	classPtr, _ := syscall.UTF16PtrFromString(classNameSystray)
+	for i := 0; i < tries; i++ {
+		hwnd := win.FindWindow(classPtr, nil)
+		if hwnd != 0 {
+			oldProc := win.SetWindowLongPtr(hwnd, win.GWLP_WNDPROC, trayWndProcPtr)
+			if oldProc != 0 {
+				trayHookMu.Lock()
+				trayHookOrigWnd = uintptr(oldProc)
+				trayHookApp = a
+				trayHookReady = true
+				trayHookMu.Unlock()
+				return
+			}
+		}
+		time.Sleep(hookRetryInterval)
+	}
+
+	_ = wmSystrayMessage
+	_ = wmLButtonDblClk
+}
+
+func trayWindowProc(hwnd win.HWND, msg uint32, wParam, lParam uintptr) uintptr {
+	const (
+		wmSystrayMessage = 0x0401
+		wmLButtonDblClk  = 0x0203
+	)
+
+	trayHookMu.Lock()
+	app := trayHookApp
+	orig := trayHookOrigWnd
+	ready := trayHookReady
+	trayHookMu.Unlock()
+
+	if ready && msg == wmSystrayMessage && lParam == wmLButtonDblClk && app != nil && app.ctx != nil {
+		go func() {
+			runtime.Show(app.ctx)
+			runtime.WindowUnminimise(app.ctx)
+			runtime.WindowShow(app.ctx)
+		}()
+	}
+
+	if orig != 0 {
+		return win.CallWindowProc(orig, hwnd, msg, wParam, lParam)
+	}
+	return win.DefWindowProc(hwnd, msg, wParam, lParam)
 }
